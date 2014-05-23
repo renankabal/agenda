b8a7550e13f64a4737429155550c64240862fd35
RENAN JHONATHA COSTA DA TRINDADE <renan@proesc.com>
2014-05-23 01:34:48 -0300

    Listagem dos Estados
    Alteração da listagem dos Estados

--- estados/lista_estados.php
+++ estados/lista_estados.php
@@ -4,27 +4,6 @@
         height:40;
         font-size:15px;
      }
-.li {
-list-style: none;
-margin: 0.5em 0 0.5em 0.5em;
-}
-.li a {
-margin:0;
-padding:0;
-text-decoration:none;
-color: #fff;
-}
-.li a:visited {
-color: #fff;
-}
-.li a:hover {
-background: #fff;
-color: #000;
-}
-.li a:active {
-background: #ccc;
-color: #000;
-}
 </style>
 <?
 $sql_estados="
@@ -44,7 +23,7 @@ while ($estados=pg_fetch_object($cons_estados))
 ?>
       <tr>
           <td><?php echo "$estados->codestado"; ?></td>
-          <td><a href='descricao.php?codestado=<?php echo "$estados->codestado"; ?>'><?php echo "$estados->estado"; ?></a></td>
+          <td><a href='descricao.php?codestado=<?php echo "$estados->codestado"; ?>' title='Selecione o estado <?php echo "$estados->estado"; ?>'><?php echo "$estados->estado"; ?></a></td>
           <td align=center>
               <a href=javascript:alterar(<?php echo "$estados->codestado"; ?>)><img src=../imagens/alterar.png title='Clique aqui para alterar'></a>
               <a href=javascript:excluir(<?php echo "$estados->codestado"; ?>)><img src=../imagens/excluir.png title='Clique aqui para excluir'></a>
